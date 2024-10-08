3510d85207d7192e8547f2ba846dd29679c8d9c2
Carlos HDL <redcarr@gmail.com>
2024-10-08 17:36:21 -0300

    meu primeiro commit

---
+++ main.py
@@ -0,0 +1,22 @@
+print("Jornada de dados")
+
+# Variaveis
+idade = 34
+nome = "Carlos"
+
+# Print
+print(idade)
+
+# soma
+nova_idade = idade + 1
+print(nova_idade)
+
+# Lista
+lista = [1,2,3,4,5]
+
+# Metodos
+lista_invertida = lista.reverse()
+
+
+
+
